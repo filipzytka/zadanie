69fcafa7c8789c2c8f224f89357170cc2d1929d5
filipus01 <filip.zytka@gmail.com>
2025-02-21 15:06:07 +0100

    refactoring the script

--- upload_s3_python.py
+++ upload_s3_python.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-"
 
-import boto_3
+from __future__ import print_function
+import boto3
 from botocore.client import ClientError
 import requests
-import subprocess
 import os
 
 # Collecting information about EC2 instance from AWS service
@@ -13,11 +13,11 @@ user_data = 'http://169.254.169.254/latest/user-data'
 meta_data = 'http://169.254.169.254/latest/meta-data'
 ec2InsDatafile = 'ec2InsDatafile'
 ec2_params = {
-    'Instance ID': 'instance-id',
-    'Reservation ID': 'reservation-id'
-    'Public IP': 'public_ipv4',
-    'Public Hostname': 'public_hostname',
-    'Private IP':'local-ipv4'
+    'Instance ID': 'i-aaabbb',
+    'Reservation ID': 'reservation-id',
+    'Public IP': '8.8.8.8',
+    'Public Hostname': 'public-hostname',
+    'Private IP':'192.168.0.1',
     'Security Groups':'security-groups',
     'AMI ID': 'ami_id'
 }
@@ -25,23 +25,23 @@ ec2_params = {
 try:
     fh = open(ec2InsDatafile, 'w')
 except:
-    print 'Error while opening file for write'
+    print('Error while opening file for write')
 
 for param, value in ec2_params.items():
     try:
-        responce = requests.get(meta_data +'/' + value)
+        response = requests.get(meta_data +'/' + value)
     except:
-        print "Error while making request"
-    if isinstance(responce.text,list):
-        print responce.text +': is list'
-        data = ' '.joint(responce.text)
+        print("Error while making request")
+    if isinstance(response.text,list):
+        print(response.text +': is list')
+        data = ' '.join(response.text)
     else:
-        data = param +":"+responce.text
+        data = param +":"+response.text
     try:
           fh.write(data+'\r\n')
     except:
         print('Error during writing to file')
-        print data
+        print(data)
 
 #Getting  OS related if from system files
 
@@ -56,7 +56,7 @@ try:
     fh.write(os_vers_val+'\r\n')
     fh.write(os_usrs_val+'\r\n')
 except:
-    print "Error during write to file"
+    print("Error during write to file")
     fh.close()
 
 
@@ -65,14 +65,15 @@ s3_bucket_name = 'new-bucket-e05ab0e0'
 s3_conn = boto3.client('s3')
 
 try:
-    s3.meta.client.head_bucket(Bucket=s3_bucket_name)
+    s3_conn.meta.client.head_bucket(Bucket=s3_bucket_name)
 
-    with (ec2InsDatafile, 'r') as fh:
+    with open(ec2InsDatafile, 'r') as fh:
+        instance_id = requests.get(meta_data + '/instance-id').text.strip() 
         s3_conn.put_object(
             Bucket=s3_bucket_name,
-            Key='system_info' + requests.get(meta_data +'/' + instance_id) + '.txt',
+            Key='system_info_' + instance_id + '.txt',
             Body=fh.read()
         )
     print("File has been uploaded into " + s3_bucket_name + " S3 bucket with instance_id key.")
 except ClientError:
-    "Are you sure the destination bucket exist? Check it."
+    print("Are you sure the destination bucket exist? Check it.")
